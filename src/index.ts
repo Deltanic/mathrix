a9a839e7bb2516fe729be1aa5c64a0401b8d9ace
Deltanic <rdg94@live.nl>
2019-11-29 00:10:51 +0100

    Add missing exports for helper functions.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import Vector2 from './primitives/Vector2';
 import Vector3 from './primitives/Vector3';
 import Vector4 from './primitives/Vector4';
-
 import Matrix4 from './primitives/Matrix4';
 import Quaternion from './primitives/Quaternion';
+
+import vec2 from './fns/vec2';
+import vec3 from './fns/vec3';
+import vec4 from './fns/vec4';
+import mat4 from './fns/mat4';
+import quat from './fns/quat';
 
 import deg from './fns/deg';
 import rad from './fns/rad';
@@ -29,6 +34,12 @@ export {
   Matrix4,
   Quaternion,
 
+  vec2,
+  vec3,
+  vec4,
+  mat4,
+  quat,
+
   deg,
   rad,
 };
@@ -36,6 +47,12 @@ export {
 export default {
   DEG_TO_RAD,
   RAD_TO_DEG,
+
+  vec2,
+  vec3,
+  vec4,
+  mat4,
+  quat,
 
   deg,
   rad,
